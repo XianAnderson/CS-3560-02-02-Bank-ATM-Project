4f46945545429e70ca9118bad74dd4a695a18f32
Christian Anderson <canderson@cpp.edu>
2024-11-14 21:55:32 -0800

    fixed the connection on dbConnect.js

--- project/src/api/dbConnect.js
+++ project/src/api/dbConnect.js
@@ -1,13 +1,30 @@
 // dbConnect.js
-const mysql = require('mysql');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: 'password',
+    password: 'your_password',
     database: 'useraccounts',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
     port: 3306
 });
 
-module.exports = connection;
+// Test function to check the database connection
+connection.getConnection((err, connection) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+    } else {
+        console.log('Database connected successfully');
+        connection.release();
+    }
+});
+
+
+// Call the test function
+//testConnection();
+
+module.exports = connection.promise();
